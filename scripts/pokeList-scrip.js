08e370a4cc79baeafcc43f9bd364251645dedad0
Debora Almeida <deboraalmeida89@gmail.com>
2024-02-01 23:53:40 -0300

    Rendering parts with observe, limiting total number and few changes on style

--- scripts/pokeList-scrip.js
+++ scripts/pokeList-scrip.js
@@ -1,4 +1,12 @@
-const getTypeColor = type => {
+let limit = 15
+let offset = 0
+let total = 1025 
+
+let controlOffset = 0
+let extra = total % limit
+let control = total - limit - extra
+
+/* const getTypeColor = type => {
   const normal = '#84918d';
   return {
     normal,
@@ -18,6 +26,84 @@ const getTypeColor = type => {
     fighting: '#31574b',
     flying: '#ace3e8'
   }[type] || normal
+} */
+
+const getTypeColor = (type, att) => {
+  const normal = {
+    color: '#84918d',
+    opacity: '#84918d99'
+  };
+  return {
+    normal,
+    fire: {
+      color: '#eb542a',
+      opacity: '#eb542a99'
+    },
+    grass: {
+      color: '#a1fc79',
+      opacity: '#a1fc7999'
+    },
+    ice: {
+      color: '#89bed9',
+      opacity: '#89bed999'
+    },
+    electric: {
+      color: '#fcc347',
+      opacity: '#fcc34799'
+    },
+    water: {
+      color: '#4270d4',
+      opacity: '#4270d499'
+    },
+    ground: {
+      color: '#695833',
+      opacity: '#69583399'
+    },
+    rock: {
+      color: '#3d382c',
+      opacity: '#3d382c99'
+    },
+    fairy: {
+      color: '#f59ab5',
+      opacity: '#f59ab599'
+    },
+    poison: {
+      color: '#9a81eb',
+      opacity: '#9a81eb99'
+    },
+    bug: {
+      color: '#acc278',
+      opacity: '#acc27899'
+    },
+    ghost: {
+      color: '#b69adb',
+      opacity: '#b69adb99'
+    },
+    dragon: {
+      color: '#97c4be',
+      opacity: '#97c4be99'
+    },
+    psychic: {
+      color: '#eaeda1',
+      opacity: '#eaeda199'
+    },
+    fighting: {
+      color: '#31574b',
+      opacity: '#31574b99'
+    },
+    flying: {
+      color: '#ace3e8',
+      opacity: '#ace3e899'
+    },
+    dark: {
+      color: '#342414',
+      opacity: '#34241499'
+    },
+    steel: {
+      color: '#2b3142',
+      opacity: '#2b314299'
+    }
+  }[type][att] || normal[att]
 }
 
 const getPokemonsInfo = async (results, info) => {
@@ -37,6 +123,12 @@ const getPokemonsInfo = async (results, info) => {
       const officalArt = sprites.map(sprite => sprite[2])
       const png = officalArt.map(png => png[1])
       return png
+      break;
+  
+    case 'sprites-home':
+      const home = sprites.map(sprite => sprite[1])
+      const homeImg = home.map(png => png[1])
+      return homeImg
       break; 
       
     case 'sprites-gif':
@@ -63,9 +155,10 @@ const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
   return urlAsArray.at(urlAsArray.length - 2)
 })
 
+
 const getPokemons = async () => {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
 
     if (!response.ok) {
       throw new Error('No Pokémon around...')
@@ -77,15 +170,24 @@ const getPokemons = async () => {
     const pokeIds = getPokemonsIds(pokeApiResults) // using this function to avoid another request in getPokemonInfo(pokeApiResults, 'id')
     const types = await getPokemonsInfo(pokeApiResults, 'types')
     const spritesPng = await getPokemonsInfo(pokeApiResults, 'sprites-png')
+    const spritesPngAlternative = await getPokemonsInfo(pokeApiResults, 'sprites-home')
     const pokemons = pokeIds.map((id, i) => (
       {
         id,
         name: pokeApiResults[i].name,
         types: types[i],
-        imgUrl: spritesPng[i].front_default
+        imgUrl: spritesPng[i].front_default !== null ? spritesPng[i].front_default : spritesPngAlternative[i].front_default
       }
-    ))
+    ))   
 
+    if (offset >= control) {
+      offset += limit
+      limit = extra
+      controlOffset += 1
+    } else { 
+      offset += limit
+    } 
+    
     return pokemons
 
   } catch (error) {
@@ -94,17 +196,15 @@ const getPokemons = async () => {
 }
 
 const renderPokemons = pokemons => {
-  //const ul = document.querySelector('[data-js="pokemons-list"]')
-  const pokeList = document.querySelector('.poke__list')
+  const pokeList = document.querySelector('[data-js="pokemons-list"]')
 
   const fragment = document.createDocumentFragment()
 
   pokemons.forEach(({ id, name, types, imgUrl }) => {
-    //const li = document.createElement('li')
     const [firstType] = types
     const pokeCard = document.createElement('div')
     pokeCard.setAttribute('class', `${firstType} poke__card`)
-    pokeCard.style.setProperty('--type-color', getTypeColor(firstType))
+    pokeCard.style.setProperty('--type-color', getTypeColor(firstType, 'color'))
 
     const pokeImg = document.createElement('img')
     pokeImg.setAttribute('class', 'poke__img')
@@ -121,7 +221,6 @@ const renderPokemons = pokemons => {
     const pokeNumber = document.createElement('h4')
     pokeNumber.setAttribute('class', '.poke__number')
     pokeNumber.textContent = `#${id}`
-
     
     const pokeInfo = document.createElement('div')
     pokeInfo.setAttribute('class', 'poke__info')
@@ -129,7 +228,7 @@ const renderPokemons = pokemons => {
     for (let i = 0; i < types.length; i++) {
       const pokeType = document.createElement('p')
       pokeType.setAttribute('class', 'poke__type')
-      pokeType.style.setProperty('--type-color', getTypeColor(types[i]))
+      pokeType.style.setProperty('--type-color', getTypeColor(types[i], 'opacity'))
       pokeType.textContent = types[i]
       pokeInfo.append(pokeType)
     }    
@@ -145,21 +244,42 @@ const renderPokemons = pokemons => {
     pokeCard.append(cardInfo)
     pokeCard.append(pokeName)
     
-    
-    console.log(pokeCard)
     fragment.append(pokeCard)    
   })
 
   pokeList.append(fragment)
 }
 
+const observeLastPokemon = pokemonObserver => {
+  const lastPokemon = document.querySelector('.poke__list').lastChild
+  pokemonObserver.observe(lastPokemon)
+}
+
+const handleNextPokemonsRender = () => {
+  const pokemonObserver = new IntersectionObserver(async ([lastPokemon], observer) => {
+    if (!lastPokemon.isIntersecting) return
+
+    observer.unobserve(lastPokemon.target)
+
+    if (controlOffset > 1) {
+      return
+    }
+
+    const pokemons = await getPokemons()
+    renderPokemons(pokemons)
+    observeLastPokemon(pokemonObserver)
+  })
+
+  observeLastPokemon(pokemonObserver)
+}
+
 const handlePageLoaded = async () => {
   const pokemons = await getPokemons()
   renderPokemons(pokemons)
+  handleNextPokemonsRender()
 }
 
 handlePageLoaded()
-
 
 /***
  * If I'd used local imgs on ./assets/img
